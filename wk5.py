09c1691893b4679b3e48f0f07fbd0a3bc6e97a67
lefteyecct <jieosullivan@yahoo.ie>
2020-10-27 22:42:05 +0000

    wk5

---
+++ wk5.py
@@ -0,0 +1,9 @@
+import datetime
+
+appolo_11_launch = datetime.date(1959, 9, 13)
+
+print (appolo_11_launch)
+
+current_datetime = datetime.date.today() # Returns datetime object of todays date
+
+print(current_datetime)
