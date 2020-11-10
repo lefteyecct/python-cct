cece4166c7ea2bb2310b975c52a5c38ab422212f
lefteyecct <jieosullivan@yahoo.ie>
2020-11-10 17:41:04 +0000

    exercises update

---
+++ exercises6.py
@@ -0,0 +1,29 @@
+"""
+    =========== Exercise 1 =============
+
+    import the math module and use math.sqrt() on
+    a user inputted variable.
+
+    NOTE: you have the input converted to an int
+    automatically using int(input()).
+"""
+
+
+"""
+    =========== Exercise 2 =============
+
+    import the sleep function from the time module as pause
+    and run pause(5)
+"""
+
+
+"""
+    =========== Exercise 3 (question) =============
+
+    import the sleep function from the time module as pause.
+
+    Write a function called pause() that takes an argument and
+    prints it.
+
+    Which pause() will be called, and why?
+"""
